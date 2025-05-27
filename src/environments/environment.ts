9816699d8588af0d08d61a533627327beeed464e
ACJ3101 <antoniocj3101@gmail.com>
2025-05-27 23:11:02 +0200

    Commit de los envitonments

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,4 @@
 export const environment = {
-  production: false,
-  apiUrl: 'http://localhost:8080/api',
-  // apiUrl: 'https://proyectointegradoback-production.up.railway.app/api'
+  production: true,
+  apiUrl: 'https://proyectointegradoback-production.up.railway.app/api'
 };
