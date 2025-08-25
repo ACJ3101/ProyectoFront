437abf1262596556eaa966280018fa165db4c1c6
ACJ3101 <antoniocj3101@gmail.com>
2025-08-25 13:06:48 +0200

    update url

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: false,
-    apiUrl: 'https://proyectointegradoback.onrender.com'
+    apiUrl: 'https://proyectointegradoback.onrender.com/api'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: true,
 
   // apiUrl: 'http://localhost:8080/api',
-  apiUrl: 'https://proyectointegradoback.onrender.com'
+  apiUrl: 'https://proyectointegradoback.onrender.com/api'
 
 };
